7894f8e40c89da6ca8a8797f3d1d161999ef7f3d
david-auk <david@aukes.com>
2025-06-24 19:15:35 +0200

    Fixed logging

--- database/03-action_types.sql
+++ database/03-action_types.sql
@@ -1,8 +1,9 @@
-INSERT INTO action_types (name, description, severity)
-VALUES ('change_allocated_ram', 'User changed the server RAM allocation', 'warning'),
-       ('edit_config_file', 'User edited a server config file', 'info'),
-       ('upload_file', 'User uploaded a file to the server', 'info'),
-       ('execute_command', 'User executed a console command', 'info'),
-       ('assign_role', 'User assigned a role to another user', 'warning'),
-       ('promote_admin', 'User promoted another user to admin', 'critical'),
-       ('demote_admin', 'User demoted admin to user', 'critical')
+INSERT INTO action_types (name, message, severity)
+VALUES ('change_allocated_ram', '${user} changed the server RAM allocation from ${from} to ${new}', 'warning'),
+       ('edit_config_file', '${user} edited a server config file', 'info'),
+       ('upload_file', '${user} uploaded a file to the server', 'info'),
+       ('execute_command', '${user} executed a console command: ${command} on ${server_instance}', 'info'),
+       ('assign_role', '${user} assigned user ${affected_user} as a ${role} on ${server_instance}', 'warning'),
+       ('promote_admin', '${user} promoted  ${affected_user} to admin', 'critical'),
+       ('demote_admin', '${user} demoted admin ${affected_user} to user', 'critical'),
+       ('user_changed_username', '${user} changed their username from ${old_username} to ${new username}', 'info')
